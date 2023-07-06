12bc863248020af73006d9112da1b2a9857bf812
mahendra-97 <104649108+mahendra-97@users.noreply.github.com>
2023-07-06 14:25:40 +0530

    Add files via upload

--- Project7/employees/models.py
+++ Project7/employees/models.py
@@ -1,10 +1,14 @@
+# from django.core.validators import validate_email
 from django.db import models
 
 class Employee(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    department = models.CharField(max_length=100)
+    # phone = models.CharField(default = 0,max_length=13, validators=[RegexValidator(r'^\+?1?\d{10}$')])
+    # email = models.EmailField(validators=[EmailValidator()])
+    phone = models.CharField(unique=True,max_length=10)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=20)
+
 
     def __str__(self):
         return self.name
--- Project7/employees/views.py
+++ Project7/employees/views.py
@@ -1,8 +1,9 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from django.views import View
-
 from .models import Employee
 
 
@@ -15,13 +16,14 @@ class EmployeeAPI(View):
                 data = {
                     'id': employee.id,
                     'name': employee.name,
+                    'phone': employee.phone,
                     'email': employee.email,
                     'department': employee.department,
                 }
             else:
                 employees = Employee.objects.all()
-                data = [{'id': emp.id, 'name': emp.name} for emp in employees]
-            
+                data = [{'id': emp.id, 'name': emp.name, 'phone': emp.phone} for emp in employees]
+
             return JsonResponse({'data': data})
         except Employee.DoesNotExist:
             return JsonResponse({'error': 'Employee not found'}, status=404)
@@ -31,10 +33,12 @@ class EmployeeAPI(View):
     def post(self, request):
         try:
             name = request.POST.get('name')
+            phone = request.POST.get('phone')
             email = request.POST.get('email')
             department = request.POST.get('department')
+            validate_email(['email'])
 
-            employee = Employee.objects.create(name=name, email=email, department=department)
+            employee = Employee.objects.create(name=name, phone=phone, email=email, department=department)
 
             return JsonResponse({'id': employee.id, 'success': 'Employee created successfully'})
         except Exception as e:
@@ -42,24 +46,26 @@ class EmployeeAPI(View):
 
     def put(self, request, employee_id):
         try:
-            name = request.POST.get('name')
-            email = request.POST.get('email')
-            department = request.POST.get('department')
+            ename = request.POST.get('name')
+            ephone = request.POST.get('phone')
+            eemail = request.POST.get('email')
+            edepartment = request.POST.get('department')
 
-            employee = Employee.objects.get(id=employee_id)
-            employee.name = name
-            employee.email = email
-            employee.department = department
-            employee.save()
+            is_employee_exist = Employee.objects.filter(id=employee_id).count()
 
-            return JsonResponse({'success': 'Employee updated successfully'})
+            if is_employee_exist > 0:
+                Employee.objects.filter(id=employee_id).update(name = ename, phone = ephone, email = eemail, department = edepartment)
+                return JsonResponse({'success': 'Employee updated successfully'})
         except Employee.DoesNotExist:
             return JsonResponse({'error': 'Employee not found'}, status=404)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
-        
-    def patch():
-        pass
+
+    def patch(self, request, employee_id):
+        try:
+            pass
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
 
     def delete(self, request, employee_id):
         try:
@@ -71,96 +77,5 @@ class EmployeeAPI(View):
             return JsonResponse({'error': 'Employee not found'}, status=404)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from django.http import JsonResponse
-# from django.views import View
-# from django.core.exceptions import ValidationError
-# from django.core.validators import validate_email
-# from .models import Employee
-
-
-# class EmployeeAPI(View):
-#     def get(self, request, employee_id=None):
-#         try:
-#             if employee_id:
-#                 employee = Employee.objects.get(id=employee_id)
-#                 data = {
-#                     'id': employee.id,
-#                     'name': employee.name,
-#                     'email': employee.email,
-#                     'department': employee.department,
-#                 }
-#             else:
-#                 employees = Employee.objects.all()
-#                 data = [{'id': emp.id, 'name': emp.name} for emp in employees]
-
-#             return JsonResponse({'data': data})
-#         except Employee.DoesNotExist:
-#             return JsonResponse({'error': 'Employee not found'}, status=404)
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=500)
-
-#     def post(self, request):
-#         try:
-#             name = request.POST.get('name')
-#             email = request.POST.get('email')
-#             department = request.POST.get('department')
-
-#             employee = Employee.objects.create(name=name, email=email, department=department)
-
-#             return JsonResponse({'id': employee.id, 'success': 'Employee created successfully'})
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=500)
-
-#     def put(self, request, employee_id):
-#         try:
-#             name = request.POST.get('name')
-#             email = request.POST.get('email')
-#             department = request.POST.get('department')
-
-#             employee = Employee.objects.get(id=employee_id)
-#             employee.name = name
-#             employee.email = email
-#             employee.department = department
-#             employee.save()
-
-#             return JsonResponse({'success': 'Employee updated successfully'})
-#         except Employee.DoesNotExist:
-#             return JsonResponse({'error': 'Employee not found'}, status=404)
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=500)
-
-#     def delete(self, request, employee_id):
-#         try:
-#             employee = Employee.objects.get(id=employee_id)
-#             employee.delete()
-
-#             return JsonResponse({'success': 'Employee deleted successfully'})
-#         except Employee.DoesNotExist:
-#             return JsonResponse({'error': 'Employee not found'}, status=404)
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=500)
-
 
 
